7cdba11cf16205f9227231ab162d83f3ee0b963a
trusteddev1409 <trusteddev1325@gmail.com>
2022-03-07 06:10:18 +0900

    version 1.0

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+import Navbar from "./components/navbar/navbar.js";
+import Header from './components/header/header';
+import Yellow from "./components/yellow/yellow";
+import Footer from "./components/footer/footer";
+function App() {
+  return (
+    <div >
+      <div className ="wrapper about ">
+        <Navbar/>
+      </div>
+        <section className="default-page default-page--about">
+          <Header/>
+          <Yellow/>
+        </section>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
